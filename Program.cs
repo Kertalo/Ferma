ec1fdc250bfd299e7f57eb80b47c1be06ba8e430
Kertalo <koksharov-kir@yandex.ru>
2023-12-14 19:57:10 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 3; // Проверяем для степени 3
+
+        for (int x = 1; x <= 100; x++)
+        {
+            for (int y = 1; y <= 100; y++)
+            {
+                for (int z = 1; z <= 100; z++)
+                {
+                    if (Math.Pow(x, n) + Math.Pow(y, n) == Math.Pow(z, n))
+                    {
+                        Console.WriteLine("Теорема Ферма верна для n={0}, x={1}, y={2}, z={3}", n, x, y, z);
+                        return;
+                    }
+                }
+            }
+        }
+        Console.WriteLine("Теорема Ферма не верна для n={0}", n);
+    }
+}
